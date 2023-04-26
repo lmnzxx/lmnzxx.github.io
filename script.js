4b75a2c6382eb22858760858155ac233dc00402e
lmnzxx <heryandi2806@gmail.com>
2023-04-26 23:15:25 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+//navbar
+var navbare = document.getElementById("navbar");
+var menu = document.getElementById("navbar-nav");
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        navbare.classList.add("sticky");
+    }
+    else{
+        navbare.classList.remove("sticky");
+    }
+}
+
+var typingEffect = new Typed(".multitext",{
+    strings : ["Mahasiswa ITB STIKOM Bali, Prodi Sistem Komputer"],
+    // loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    startDelay : 1000,
+    backDelay : 1500
+})
+
+gsap.from('.konten p',{ duration: 1, x: -300, opacity: 0, ease: 'expo'});
+gsap.from('.konten h1',{ duration: 2, x: -300, opacity: 0, ease: 'expo'});
+gsap.from('.konten',{ duration: 2.5, x: -300, opacity: 0, ease: 'expo'});
+
+gsap.from('.navbar',{ duration: 2, y: -100, opacity: 0, ease: 'expo'});
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+    wrapper: '#navbar',
+    content: '#smooth-konten',
+    smooth: 2,
+    effects: true
+})
+
+

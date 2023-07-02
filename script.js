eb2346ee071d189a63bf5bd6107fd09a3a11c7d4
Heryandi Pradana <heryandi2806@gmail.com>
2023-07-03 05:05:09 +0800

    Final

--- script.js
+++ script.js
@@ -24,3 +24,4 @@ gsap.from('.konten p',{ duration: 1, x: -300, opacity: 0, ease: 'expo'});
 gsap.from('.konten h1',{ duration: 2, x: -300, opacity: 0, ease: 'expo'});
 gsap.from('.konten',{ duration: 2.5, x: -300, opacity: 0, ease: 'expo'});
 gsap.from('.navbar',{ duration: 2, y: -100, opacity: 0, ease: 'expo'});
+
